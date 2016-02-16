c96056ffdb2fbf47dd92b4544ab8c0635d927e10
tsuchimore1 <yoshi1003to216@gmail.com>
2016-02-16 11:06:06 +0000

    first

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,8 @@ Rails.application.routes.draw do
   post   'login' , to: 'sessions#create'
   get  'Settings', to: 'users#edit'
   delete 'logout', to: 'sessions#destroy'
-  
 
   resources :users
-
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts
 end
